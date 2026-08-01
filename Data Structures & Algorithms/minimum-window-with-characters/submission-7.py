6f06b92f12953c5948d78b2a07c295aad4827026
GarabKDorji <121268256+GarabKDorji@users.noreply.github.com>
2026-08-01 15:39:17 -0400

    Add: minimum-window-with-characters - submission-7

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-7.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if len(s) < len(t):
+            return ""
+
+        freq_t = Counter(t)
+        res = [-1,-1]
+        shortest = float("inf")
+        for i in range(len(s)):
+            freq_s = defaultdict(int)
+            for j in range(i,len(s)):
+                freq_s[s[j]] += 1 
+                
+                found = True 
+                for k in freq_t: 
+                    if freq_s[k] < freq_t[k]:
+                        found = False 
+                        break 
+
+                if found and shortest > j - i + 1 :
+                    res = [i,j]
+                    shortest = j - i +1
+
+        l ,r = res 
+        return s[l :r+1]
