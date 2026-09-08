0248ee803efff3b2a3592561098e8e25b91d818f
Garab Kuenphen Dorji <121268256+GarabKDorji@users.noreply.github.com>
2026-09-08 17:28:10 -0400

    Add: lru-cache - submission-6

---
+++ Data Structures & Algorithms/lru-cache/submission-6.py
@@ -0,0 +1,45 @@
+class Node: 
+
+    def __init__(self, key, val):
+        self.key = key 
+        self.val = val
+        self.prev = None 
+        self.next = None
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.cap = capacity 
+        self.store = {}
+        self.left, self.right = Node(0,0), Node(0,0) 
+        self.left.next, self.right.prev  = self.right, self.left
+
+    def remove(self, node):
+        prev, nxt = node.prev , node.next 
+        prev.next , nxt.prev = nxt, prev
+    
+    def insert(self, node):
+        prev, nxt = self.right.prev , self.right 
+        prev.next , self.right.prev = node , node 
+        node.next, node.prev = nxt , prev
+
+
+    def get(self, key: int) -> int:
+        if key not in self.store: 
+            return -1
+        node = self.store[key]
+        self.remove(node)
+        self.insert(node)
+        return node.val
+
+    
+    def put(self, key: int, value: int) -> None:
+        if key in self.store:
+            self.remove(self.store[key])
+        self.store[key]= Node(key,value)
+        self.insert(self.store[key])
+
+        if len(self.store) > self.cap: 
+            lru = self.left.next 
+            del self.store[lru.key]
+            self.remove(lru)
+        
